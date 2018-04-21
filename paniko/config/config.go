9e9a1908312081640be97c799c9d6f99d4160bb6
Christopher John Jackson <hello@cj-jackson.com>
2018-04-21 17:04:30 +0100

    Got expiration sorted out

--- paniko/config/config.go
+++ paniko/config/config.go
@@ -9,7 +9,7 @@ import (
 )
 
 type Config struct {
-	DaysToExpiry int64
+	DaysToExpiry int
 	Mail         Mail
 }
 
---
+++ paniko/expiration/expiration.go
@@ -0,0 +1,67 @@
+package expiration
+
+import (
+	"sync"
+	"time"
+
+	"github.com/CJ-Jackson/ctx"
+	"github.com/CJ-Jackson/paniko/paniko/config"
+)
+
+type Expiration interface {
+	GetTime() time.Time
+	Expired() bool
+	Reset()
+	Expire()
+}
+
+func GetExpiration(context ctx.BackgroundContext) Expiration {
+	name := "expiration-fa22848f098081e710887c8a2b930f07"
+	if e, ok := context.Ctx(name).(Expiration); ok {
+		return e
+	}
+
+	daysToExpire := config.GetConfig(context).DaysToExpiry
+
+	e := &expiration{
+		daysToExpire: daysToExpire,
+		t:            time.Now().AddDate(0, 0, daysToExpire),
+	}
+
+	context.SetCtx(name, e)
+	return e
+}
+
+type expiration struct {
+	sync.RWMutex
+	daysToExpire int
+	t            time.Time
+}
+
+func (e *expiration) GetTime() time.Time {
+	e.RLock()
+	defer e.RUnlock()
+
+	return e.t
+}
+
+func (e *expiration) Expired() bool {
+	e.RLock()
+	defer e.RUnlock()
+
+	return time.Now().After(e.t)
+}
+
+func (e *expiration) Reset() {
+	e.Lock()
+	defer e.Unlock()
+
+	e.t = time.Now().AddDate(0, 0, e.daysToExpire)
+}
+
+func (e *expiration) Expire() {
+	e.Lock()
+	defer e.Unlock()
+
+	e.t = time.Now()
+}

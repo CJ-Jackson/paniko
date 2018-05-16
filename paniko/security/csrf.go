d4db5922ab657ced3251459008900d18a1b80d36
Christopher John Jackson <hello@cj-jackson.com>
2018-05-16 15:20:23 +0100

    Clean up security

--- paniko/security/csrf.go
+++ paniko/security/csrf.go
@@ -11,34 +11,30 @@ import (
 )
 
 func GetCsrf(context ctx.BackgroundContext) common.ContextHandler {
-	const name = "csrf-7cf4ddd5429f9237ebd331a9c65498ed"
-	if contextHandler, ok := context.Get(name).(common.ContextHandler); ok {
-		return contextHandler
-	}
+	return context.Persist("csrf-7cf4ddd5429f9237ebd331a9c65498ed", func() (interface{}, error) {
+		csrfProtect := csrf.Protect(
+			[]byte(common.GetConfig(context).CsrfKey),
+			csrf.Secure(false),
+			csrf.ErrorHandler(http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
+				common.HaltForbidden("Invalid Csrf Token")
+			})),
+		)
 
-	csrfProtect := csrf.Protect(
-		[]byte(common.GetConfig(context).CsrfKey),
-		csrf.Secure(false),
-		csrf.ErrorHandler(http.HandlerFunc(func(_ http.ResponseWriter, _ *http.Request) {
-			common.HaltForbidden("Invalid Csrf Token")
-		})),
-	)
+		contextHandler := func(context ctx.Context) {
+			context.PersistData(shared.CsrfDataName, func() interface{} {
+				var data csrfData
+				csrfProtect(http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
+					data = csrfData{
+						tokenField: csrf.TemplateField(req),
+						token:      csrf.Token(req),
+					}
+				})).ServeHTTP(context.ResponseWriter(), context.Request())
 
-	contextHandler := func(context ctx.Context) {
-		if _, ok := context.Data(shared.CsrfDataName).(csrfData); ok {
-			return
+				return data
+			})
 		}
-
-		csrfProtect(http.HandlerFunc(func(_ http.ResponseWriter, req *http.Request) {
-			context.SetData(shared.CsrfDataName, shared.Csrf(csrfData{
-				tokenField: csrf.TemplateField(req),
-				token:      csrf.Token(req),
-			}))
-		})).ServeHTTP(context.ResponseWriter(), context.Request())
-	}
-
-	context.Set(name, contextHandler)
-	return contextHandler
+		return contextHandler, nil
+	}).(common.ContextHandler)
 }
 
 type csrfData struct {

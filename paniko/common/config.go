b8a9fdab13c17fbb16a6e6372f882cb99097557c
Christopher John Jackson <hello@cj-jackson.com>
2018-05-16 14:02:38 +0100

    wip

--- paniko/common/config.go
+++ paniko/common/config.go
@@ -10,44 +10,40 @@ import (
 )
 
 func GetConfig(context ctx.BackgroundContext) config.Base {
-	const name = "config-5a43ef4f8f6dbc0ee0ec3471d26dfdcd"
-	if configVar, ok := context.Get(name).(config.Base); ok {
-		return configVar
-	}
+	return context.Persist("config-5a43ef4f8f6dbc0ee0ec3471d26dfdcd", func() (interface{}, error) {
+		configVar := config.Base{
+			DaysToExpiry: 7,
+			CsrfKey:      "",
+			Mail: config.Mail{
+				Username: "",
+				Password: "",
+				Hostname: "",
+				Port:     25,
+				From:     mail.Address{},
+				SendTo:   []mail.Address{},
+				SendCc:   []mail.Address{},
+				Subject:  "",
+			},
+			Password: config.Password{
+				Salt:     "",
+				Location: os.Getenv("HOME") + "/.config/paniko/password.json",
+			},
+			Cookie: config.Cookie{
+				HashKey:  "",
+				BlockKey: "",
+			},
+		}
+		location := os.Getenv("HOME") + "/.config/paniko/config.json"
 
-	configVar := config.Base{
-		DaysToExpiry: 7,
-		CsrfKey:      "",
-		Mail: config.Mail{
-			Username: "",
-			Password: "",
-			Hostname: "",
-			Port:     25,
-			From:     mail.Address{},
-			SendTo:   []mail.Address{},
-			SendCc:   []mail.Address{},
-			Subject:  "",
-		},
-		Password: config.Password{
-			Salt:     "",
-			Location: os.Getenv("HOME") + "/.config/paniko/password.json",
-		},
-		Cookie: config.Cookie{
-			HashKey:  "",
-			BlockKey: "",
-		},
-	}
-	location := os.Getenv("HOME") + "/.config/paniko/config.json"
+		errorService := GetErrorService(context)
 
-	errorService := GetErrorService(context)
+		file, err := os.Open(location)
+		errorService.CheckErrorAndPanic(err)
+		defer file.Close()
 
-	file, err := os.Open(location)
-	errorService.CheckErrorAndPanic(err)
-	defer file.Close()
+		err = json.NewDecoder(file).Decode(&configVar)
+		errorService.CheckErrorAndPanic(err)
 
-	err = json.NewDecoder(file).Decode(&configVar)
-	errorService.CheckErrorAndPanic(err)
-
-	context.Set(name, configVar)
-	return configVar
+		return configVar, nil
+	}).(config.Base)
 }
--- paniko/common/message.go
+++ paniko/common/message.go
@@ -8,23 +8,19 @@ import (
 )
 
 func GetMessage(context ctx.BackgroundContext) string {
-	const name = "message-071262aa97887e3b10cf6c00d7462f24"
-	if message, ok := context.Get(name).(string); ok {
-		return message
-	}
+	return context.Persist("message-071262aa97887e3b10cf6c00d7462f24", func() (interface{}, error) {
+		location := os.Getenv("HOME") + "/.config/paniko/message.txt"
 
-	location := os.Getenv("HOME") + "/.config/paniko/message.txt"
+		errorService := GetErrorService(context)
 
-	errorService := GetErrorService(context)
+		file, err := os.Open(location)
+		errorService.CheckErrorAndPanic(err)
 
-	file, err := os.Open(location)
-	errorService.CheckErrorAndPanic(err)
+		b, err := ioutil.ReadAll(file)
+		errorService.CheckErrorAndPanic(err)
 
-	b, err := ioutil.ReadAll(file)
-	errorService.CheckErrorAndPanic(err)
+		message := string(b)
 
-	message := string(b)
-
-	context.Set(name, message)
-	return message
+		return message, nil
+	}).(string)
 }
